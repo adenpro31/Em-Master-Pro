2d19e42ecd0803f9b88d385198865475c9f54a7e
adenpro31 <adenacarturka@gmail.com>
2025-04-07 14:31:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+window.onload = function () {
+    setTimeout(() => {
+        document.getElementById('splash').style.display = 'none';
+        document.getElementById('app').style.display = 'block';
+    }, 2500);
+};
